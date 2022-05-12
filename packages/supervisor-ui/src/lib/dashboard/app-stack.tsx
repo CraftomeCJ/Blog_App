9416a997ece7e8fd6d7073b81d0f9606e14bf562
Anya Huang <huang.anya@activate.sg>
2022-05-12 11:29:24 +0800

    remove the unnecessary import from app-stack

--- packages/supervisor-ui/src/lib/dashboard/app-stack.tsx
+++ packages/supervisor-ui/src/lib/dashboard/app-stack.tsx
@@ -1,10 +1,6 @@
-import React, { ReactElement } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import DashboardScreen from './screens/dashboard';
 import DetailScreen from './screens/detail';
 
-const Stack = createStackNavigator<{ Dashboard: undefined }>();
 const AppStack = [
   {
     name: 'Dashboard',
@@ -20,23 +16,6 @@ const AppStack = [
       title: 'Supervisor Detail',
     },
   },
-];
-const AppStack1 = [
-  <Stack.Screen
-    name={'Dashboard'}
-    component={DashboardScreen}
-    options={{
-      title: 'Supervisor Dashboard1111',
-    }}
-  />,
-  <Stack.Screen
-    name={'Detail'}
-    component={DetailScreen}
-    options={{
-      title: 'Supervisor Detail',
-      headerLeft: () => null,
-    }}
-  />,
 ];
 
 export default AppStack;
