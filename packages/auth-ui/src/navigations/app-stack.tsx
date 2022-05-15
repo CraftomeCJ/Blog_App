ae1aece1ab651dd58cb9ef77dca2cd44f6eaa2ea
Anya Huang <huang.anya@activate.sg>
2022-05-16 00:46:26 +0800

    remove the left header button

--- packages/auth-ui/src/navigations/app-stack.tsx
+++ packages/auth-ui/src/navigations/app-stack.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useAuth } from '@shareapp1/common-utils';
 import { authScreens } from './auth-screens';
@@ -6,16 +6,18 @@ import { authScreens } from './auth-screens';
 const Stack = createStackNavigator<{ Welcome: undefined }>();
 export const AuthStack = () => {
   const { user } = useAuth();
-  useEffect(() => {
-    console.log('AuthStack auth', user);
-  }, [user]);
   if (user) {
     return null;
   }
   return (
     <Stack.Navigator
       initialRouteName={authScreens[0].name}
-      screenOptions={{ gestureEnabled: false }}
+      screenOptions={{
+        gestureEnabled: false,
+        headerLeft: () => {
+          return null;
+        },
+      }}
     >
       {authScreens.map((screen) => (
         <Stack.Screen
